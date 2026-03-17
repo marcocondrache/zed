8f247561006e2e185a83fe6f3a6ba87c2a3ec585
dino <dinojoaocosta@gmail.com>
2026-03-17 17:12:40 +0000

    chore(project_panel): show error notification when undo fails
    * Update `project_panel::undo::UndoManager::revert_operation` in order
      to return a vector of errors. This way we can determine whether
      undoing the operation failed and return multiple errors in the case of
      a `ProjectPanelOperation::Batch`.
    * Introduce `project_panel::undo::UndoManager::show_errors` to display a
      workspace notification with all of the errors that originated when
      attempting to undo a project panel operation.
      * Since we need to be aware of the workspace in order to be able to
        display a workspace notification, `UndoManager::workspace` has also
        been introduced

--- crates/project_panel/src/undo.rs
+++ crates/project_panel/src/undo.rs
@@ -1,8 +1,12 @@
-use anyhow::{Result, anyhow};
+use anyhow::anyhow;
 use circular_buffer::CircularBuffer;
-use gpui::{Entity, Task};
+use gpui::{AppContext, Entity, SharedString, Task, WeakEntity};
 use project::{Project, ProjectPath};
-use ui::App;
+use ui::{App, IntoElement, Label, ParentElement, Styled, v_flex};
+use workspace::{
+    Workspace,
+    notifications::{NotificationId, simple_message_notification::MessageNotification},
+};
 
 const MAX_UNDO_OPERATIONS: usize = 10_000;
 
@@ -30,20 +34,38 @@ impl ProjectPanelOperation {
 
 pub struct UndoManager {
     project: Entity<Project>,
+    workspace: WeakEntity<Workspace>,
     stack: Box<CircularBuffer<MAX_UNDO_OPERATIONS, ProjectPanelOperation>>,
 }
 
 impl UndoManager {
-    pub fn new(project: Entity<Project>) -> Self {
+    pub fn new(project: Entity<Project>, workspace: WeakEntity<Workspace>) -> Self {
         Self {
             project,
+            workspace,
             stack: CircularBuffer::boxed(),
         }
     }
 
     pub fn undo(&mut self, cx: &mut App) {
         if let Some(operation) = self.stack.pop_back() {
-            self.revert_operation(operation, cx).detach_and_log_err(cx);
+            let task = self.revert_operation(operation, cx);
+            let workspace = self.workspace.clone();
+
+            cx.spawn(async move |cx| {
+                let errors = task.await;
+                if !errors.is_empty() {
+                    cx.update(|cx| {
+                        let messages = errors
+                            .iter()
+                            .map(|err| SharedString::from(err.to_string()))
+                            .collect();
+
+                        Self::show_errors(workspace, messages, cx)
+                    })
+                }
+            })
+            .detach();
         }
     }
 
@@ -53,7 +75,16 @@ impl UndoManager {
         }
     }
 
-    fn revert_operation(&self, operation: ProjectPanelOperation, cx: &mut App) -> Task<Result<()>> {
+    /// Attempts to revert the provided `operation`, returning a vector of errors
+    /// in case there was any failure while reverting the operation.
+    ///
+    /// For all operations other than [`crate::undo::ProjectPanelOperation::Batch`], a maximum
+    /// of one error is returned.
+    fn revert_operation(
+        &self,
+        operation: ProjectPanelOperation,
+        cx: &mut App,
+    ) -> Task<Vec<anyhow::Error>> {
         match operation {
             ProjectPanelOperation::Create { project_path } => {
                 let Some(entry_id) = self
@@ -62,15 +93,19 @@ impl UndoManager {
                     .entry_for_path(&project_path, cx)
                     .map(|e| e.id)
                 else {
-                    return Task::ready(Err(anyhow!("no entry for path")));
+                    return Task::ready(vec![anyhow!("No entry for path.")]);
                 };
                 let Some(task) = self
                     .project
                     .update(cx, |project, cx| project.delete_entry(entry_id, true, cx))
                 else {
-                    return Task::ready(Err(anyhow!("failed to trash entry")));
+                    return Task::ready(vec![anyhow!("Failed to trash entry.")]);
                 };
-                cx.spawn(async move |_cx| task.await.map(|_| ()))
+
+                cx.spawn(async move |_| match task.await {
+                    Ok(_) => vec![],
+                    Err(err) => vec![anyhow!(err)],
+                })
             }
             ProjectPanelOperation::Rename { old_path, new_path } => {
                 let Some(entry_id) = self
@@ -79,12 +114,16 @@ impl UndoManager {
                     .entry_for_path(&new_path, cx)
                     .map(|e| e.id)
                 else {
-                    return Task::ready(Err(anyhow!("no entry for path")));
+                    return Task::ready(vec![anyhow!("no entry for path")]);
                 };
                 let task = self.project.update(cx, |project, cx| {
                     project.rename_entry(entry_id, old_path.clone(), cx)
                 });
-                cx.spawn(async move |_| task.await.map(|_| ()))
+
+                cx.spawn(async move |_| match task.await {
+                    Ok(_) => vec![],
+                    Err(err) => vec![anyhow!(err)],
+                })
             }
             ProjectPanelOperation::Batch(operations) => {
                 let tasks: Vec<_> = operations
@@ -92,17 +131,56 @@ impl UndoManager {
                     .map(|op| self.revert_operation(op, cx))
                     .collect();
 
+                // TODO!: Update to use a sequential approach instead of
+                // parallel tasks, and to collect the errors so we can display
+                // all in an error message.
                 cx.spawn(async move |_| {
                     let results = futures::future::join_all(tasks).await;
-                    let errors: Vec<_> = results.into_iter().filter_map(|r| r.err()).collect();
-                    // TODO: better understand what to do with these errors
-                    if errors.is_empty() {
-                        Ok(())
-                    } else {
-                        Err(anyhow!("Some operations failed"))
-                    }
+                    let errors: Vec<_> = results
+                        .into_iter()
+                        .filter(|errors| !errors.is_empty())
+                        .flatten()
+                        .collect();
+
+                    if errors.is_empty() { vec![] } else { errors }
                 })
             }
         }
+    }
+
+    /// Displays a notification with the list of provided errors ensuring that,
+    /// when more than one error is provided, which can be the case when dealing
+    /// with undoing a [`crate::undo::ProjectPanelOperation::Batch`], a list is
+    /// displayed with each of the errors, instead of a single message.
+    fn show_errors(workspace: WeakEntity<Workspace>, messages: Vec<SharedString>, cx: &mut App) {
+        workspace
+            .update(cx, move |workspace, cx| {
+                let notification_id =
+                    NotificationId::Named(SharedString::new_static("project_panel_undo"));
+
+                workspace.show_notification(notification_id, cx, move |cx| {
+                    let messages = messages.clone();
+
+                    cx.new(|cx| {
+                        if let [err] = messages.as_slice() {
+                            MessageNotification::new(err.to_string(), cx)
+                                .with_title("Failed to undo Project Panel Operation")
+                        } else {
+                            MessageNotification::new_from_builder(cx, move |_, _| {
+                                v_flex()
+                                    .gap_1()
+                                    .children(
+                                        messages
+                                            .iter()
+                                            .map(|message| Label::new(format!("- {message}"))),
+                                    )
+                                    .into_any_element()
+                            })
+                            .with_title("Failed to undo Project Panel Operations")
+                        }
+                    })
+                })
+            })
+            .ok();
     }
 }
